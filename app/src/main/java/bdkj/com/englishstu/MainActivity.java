7bb53e92566a792105c4e041a98733c2c073e167
徐伟 <xuwei@njbandou.com>
2017-05-25 10:38:23 +0800

    测试修改提交

---
+++ app/src/main/java/bdkj/com/englishstu/MainActivity.java
@@ -0,0 +1,15 @@
+package bdkj.com.englishstu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.e("HHH","hhh");
+        setContentView(R.layout.activity_main);
+    }
+}
